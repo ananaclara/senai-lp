967b7b6939e8f058dc376302eef5e67229a6dd92
GuidoGuarda <dcguidosenai@gmail.com>
2024-09-27 21:41:11 -0300

    exe2_3.4.js

--- livrosjs/cap02/js/exe2_2.js
+++ livrosjs/cap02/js/exe2_2.js
@@ -1,16 +1,13 @@
 const frm = document.querySelector("form")
 const resp1 = document.querySelector("h3")
 const resp2 = document.querySelector("h4")
-
 frm.addEventListener("submit", (e)=>{
-    const titulo = frm.inTitulo,value; // entrada de dados
-    const duracao = frn.inDuracao,value; // entrada de dados
+    const titulo = frm.inTitulo.value; // entrada de dados
+    const duracao = frm.inDuracao.value; // entrada de dados
     //processamento
-    const horas = math.floor(duracao / 60) ; //processamento
+    const horas = Math.floor(duracao / 60) ; //processamento
     const minutos = duracao % 60 // processamento
-
-    resp1.innerText - titulo; // saida
-    resp2,innerText = `${horas} horas(s)e ${minutos} minuto(s)`; // saida
-
+    resp1.innerText = titulo; // saida
+    resp2.innerText = `${horas} horas(s) e ${minutos} minuto(s)`; // saida
     e.preventDefault();
 })
