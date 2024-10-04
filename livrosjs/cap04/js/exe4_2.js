1ed23cec59a026dc371258b7b393d48aee72b79b
ana <anaclaraoliveiraes@gmail.com>
2024-10-03 21:07:09 -0300

    exemplo4.3

--- livrosjs/cap04/js/exe4_2.js
+++ livrosjs/cap04/js/exe4_2.js
@@ -1,13 +1,17 @@
 function calcular() {
     const nome = document.getElementById("inNome").value;
     const masculino = document.getElementById("inMasculino").checked;
-    const altura = document.getElementById("inAltura").checked;
-let peso;
- if(masculino){
-    peso = 22 * Math.pow(altura, 2)
-}else(
-    peso = 21 * Math.pow(altura, 2)
-)
-document.querySelector("h3").innerText = `Peso ideal ${peso.toFixed(2)}`;
-return false;
+    const altura = document.getElementById("inAltura").value;
+    let peso = masculino ? Math.pow(altura, 2) * 22 : Math.pow(altura, 2) * 21;
+    /*
+     if(masculino){
+        peso = 22 * Math.pow (altura, 2)
+    }else{
+        peso = 21 * Math.pow (altura, 2)
+    }*/
+    document.querySelector("h3").innerText = `${nome}: Peso ideal ${peso.toFixed(2)}`;
+    return false;
+}
+function limpar() {
+    document.querySelector("h3").innerText = "";
 }
